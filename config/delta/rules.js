2bac15fc46870ed7b089cd94e11b82c7802813ee
Erika Pauwels <erika.pauwels@gmail.com>
2019-09-26 17:14:09 +0200

    Configure delta-notifier for download-uri service

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -4,13 +4,28 @@ export default [
       subject: { }
     },
     callback: {
-      url: "http://resource/.mu/delta",
-      method: "POST"
+      url: 'http://resource/.mu/delta',
+      method: 'POST'
     },
     options: {
-      resourceFormat: "v0.0.1",
+      resourceFormat: 'v0.0.1',
       gracePeriod: 250,
       ignoreFromSelf: true
     }
+  },
+  {
+    match: {
+      predicate: 'http://www.w3.org/ns/adms#status',
+      object: 'http://lblod.data.gift/file-download-statuses/ready-to-be-cached'
+    },
+    callback: {
+      url: 'http://download-url/process-remote-data-objects',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
   }
-]
+];
