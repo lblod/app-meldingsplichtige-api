f4e6c45d338efb436f7312ef7cc1525bfda0d6b9
Erika Pauwels <erika.pauwels@gmail.com>
2020-01-09 22:28:25 +0100

    Add validate submission service
    Forms are still a work in progress

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -54,5 +54,26 @@ export default [
       gracePeriod: 1000,
       ignoreFromSelf: true
     }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      },
+      object: {
+        type: 'uri',
+        value: 'http://lblod.data.gift/automatische-melding-statuses/ready-for-validation'
+      }
+    },
+    callback: {
+      url: 'http://validate-submission/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
   }
 ];
