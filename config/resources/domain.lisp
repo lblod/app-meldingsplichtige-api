96f9324b8f590a08eca51940393fa635e456d4cb
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-10 16:46:39 +0100

    included "file" model in resources + configured authorization to allow for public writing of files.

--- config/resources/domain.lisp
+++ config/resources/domain.lisp
@@ -15,3 +15,16 @@
 (read-domain-file "slave-besluit-domain.lisp")
 (read-domain-file "master-users-domain.lisp")
 
+(define-resource file ()
+  :class (s-prefix "nfo:FileDataObject")
+  :properties `((:filename :string ,(s-prefix "nfo:fileName"))
+                (:format :string ,(s-prefix "dct:format"))
+                (:size :number ,(s-prefix "nfo:fileSize"))
+                (:extension :string ,(s-prefix "dbpedia:fileExtension"))
+                (:created :datetime ,(s-prefix "nfo:fileCreated")))
+  :has-one `((file :via ,(s-prefix "nie:dataSource")
+                   :inverse t
+                   :as "download"))
+  :resource-base (s-url "http://data.lblod.info/files/")
+  :features `(include-uri)
+  :on-path "files")
