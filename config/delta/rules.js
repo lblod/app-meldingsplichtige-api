1935ded9830cd23f192d7c16f6bd6f3a6124b55c
Erika Pauwels <erika.pauwels@gmail.com>
2020-03-04 15:24:28 +0100

    Add enrich-submission service

--- config/delta/rules.js
+++ config/delta/rules.js
@@ -77,6 +77,27 @@ export default [
       },
       object: {
         type: 'uri',
+        value: 'http://lblod.data.gift/automatische-melding-statuses/ready-for-enrichment'
+      }
+    },
+    callback: {
+      url: 'http://enrich-submission/delta',
+      method: 'POST'
+    },
+    options: {
+      resourceFormat: 'v0.0.1',
+      gracePeriod: 1000,
+      ignoreFromSelf: true
+    }
+  },
+  {
+    match: {
+      predicate: {
+        type: 'uri',
+        value: 'http://www.w3.org/ns/adms#status'
+      },
+      object: {
+        type: 'uri',
         value: 'http://lblod.data.gift/automatische-melding-statuses/ready-for-validation'
       }
     },
