e8512680be3107b97c9a8945967020c93de72907
Erika Pauwels <erika.pauwels@gmail.com>
2020-03-24 10:08:03 +0100

    Bump enrich submission service

--- config/resources/repository.lisp
+++ config/resources/repository.lisp
@@ -22,3 +22,4 @@
 (add-prefix "rdfs" "https://www.w3.org/2000/01/rdf-schema#")
 (add-prefix "org" "http://www.w3.org/ns/org#")
 (add-prefix "nmo" "http://www.semanticdesktop.org/ontologies/2007/03/22/nmo#")
+
