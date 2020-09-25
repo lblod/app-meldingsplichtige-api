cba437068f33f775fbdd0589201293255ee44620
achraf <achrafatauil@hotmail.com>
2020-09-25 14:33:34 +0200

    Add vendor has-many bestuurseenheid relationship
    This is needed for the vendor-key-management feature

--- config/resources/master-submissions-domain.lisp
+++ config/resources/master-submissions-domain.lisp
@@ -50,6 +50,8 @@
   :class (s-prefix "ext:Vendor") ; Subclass of foaf:Agent
   :properties `((:name :string ,(s-prefix "foaf:name"))
                 (:key :string ,(s-prefix "muAccount:key")))
+  :has-many `((bestuurseenheid :via ,(s-prefix "muAccount:canActOnBehalfOf")
+                    :as "can-act-on-behalf"))
   :resource-base (s-url "http://data.lblod.info/vendors/")
   :features `(include-uri)
   :on-path "vendors")
