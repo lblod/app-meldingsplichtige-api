9c5f13af1ee091a65639ef56d765d80b7c2e2a2f
achraf <achrafatauil@hotmail.com>
2020-10-15 18:00:56 +0200

    remove inverse from hasMany vendor > bestuurseenheid

--- config/resources/master-submissions-domain.lisp
+++ config/resources/master-submissions-domain.lisp
@@ -51,7 +51,6 @@
   :properties `((:name :string ,(s-prefix "foaf:name"))
                 (:key :string ,(s-prefix "muAccount:key")))
   :has-many `((bestuurseenheid :via ,(s-prefix "muAccount:canActOnBehalfOf")
-                    :inverse t
                     :as "can-act-on-behalf-of"))
   :resource-base (s-url "http://data.lblod.info/vendors/")
   :features `(include-uri)
