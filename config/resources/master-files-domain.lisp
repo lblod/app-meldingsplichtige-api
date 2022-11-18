fb0d5761d30c069424bf6e5103f666db39bccbad
Sergey Andreev <sergey.andreev@redpencil.io>
2022-11-18 16:23:52 +0100

    fixed extra line

--- config/resources/master-files-domain.lisp
+++ config/resources/master-files-domain.lisp
@@ -2,7 +2,6 @@
   :class (s-prefix "nfo:FileDataObject")
   :properties `((:filename      :string   ,(s-prefix "nfo:fileName"))
                 (:name      :string   ,(s-prefix "nfo:fileName"))
-                (:name      :string   ,(s-prefix "nfo:fileName"))
                 (:format    :string   ,(s-prefix "dct:format"))
                 (:size      :number   ,(s-prefix "nfo:fileSize"))
                 (:extension :string   ,(s-prefix "dbpedia:fileExtension"))
